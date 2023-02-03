26bffc5eda32a7c184a674d961da6746d107ca51
suzzeong <su981006@naver.com>
2023-02-04 00:00:03 +0900

    feat: DM 페이지 만들기

---
+++ front/components/ChatList/index.tsx
@@ -0,0 +1,12 @@
+import { ChatZone, Section } from './styles';
+import React from 'react';
+
+const ChatList = () => {
+  return (
+    <ChatZone>
+      <Section></Section>
+    </ChatZone>
+  );
+};
+
+export default ChatList;
--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -1,7 +1,31 @@
+import { Container, Header } from '@pages/DirectMessage/styles';
 import React from 'react';
+import gravatar from 'gravatar';
+import { IUser } from '@typings/db';
+import fetcher from '@utils/fetcher';
+import useSWR from 'swr';
+import { useParams } from 'react-router-dom';
+import ChatBox from '@components/ChatBox';
 
 const DirectMessage = () => {
-  return <div>DM 페이지</div>;
+  const { workspace, id } = useParams<{ workspace?: string; id: string }>();
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+  const { data: myData } = useSWR(`/api/users`, fetcher);
+
+  if (!userData || !myData) {
+    return null;
+  }
+
+  return (
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '28px', d: 'retro' })} alt={userData.nickname} />
+        <span>{userData.nickname}</span>
+      </Header>
+      {/* <ChatList/> */}
+      <ChatBox chat="" />
+    </Container>
+  );
 };
 
 export default DirectMessage;
