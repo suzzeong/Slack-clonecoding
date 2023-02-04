c23af330f74f42f4ab9e55f0c3eb4780cc234782
suzzeong <su981006@naver.com>
2023-02-04 17:21:01 +0900

    feat: Chat 보내기

--- front/components/ChatBox/index.tsx
+++ front/components/ChatBox/index.tsx
@@ -24,7 +24,7 @@ const ChatBox: VFC<Props> = ({ chat, onSubmitForm, onChangeChat, placeholder }) 
         onSubmitForm(e);
       }
     }
-  }, []);
+  }, [onSubmitForm]);
 
   return (
     <ChatArea>
--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -1,27 +1,49 @@
 import { Container, Header } from '@pages/DirectMessage/styles';
 import React, { useCallback } from 'react';
 import gravatar from 'gravatar';
-import { IUser } from '@typings/db';
+import { IDM, IUser } from '@typings/db';
 import fetcher from '@utils/fetcher';
 import useSWR from 'swr';
 import { useParams } from 'react-router-dom';
 import ChatBox from '@components/ChatBox';
 import ChatList from '@components/ChatList';
 import useInput from '@hooks/useInput';
+import axios from 'axios';
 
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace?: string; id: string }>();
   const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
   const { data: myData } = useSWR(`/api/users`, fetcher);
+  const { data: chatData, mutate: mutateChat } = useSWR<IDM[]>(
+    `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`,
+    fetcher,
+  );
 
-  const [chat, onChangeChat] = useInput('')
+  const [chat, onChangeChat, setChat] = useInput('');
 
-  const onSubmitForm = useCallback(() => {}, []);
+  const onSubmitForm = useCallback(
+    (e) => {
+      e.preventDefault();
+      console.log('chat');
+
+      if (chat?.trim()) {
+        axios
+          .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
+            content: chat,
+          })
+          .then(() => {
+            mutateChat();
+            setChat('');
+          })
+          .catch(console.error);
+      }
+    },
+    [chat],
+  );
 
   if (!userData || !myData) {
     return null;
   }
-
 
   return (
     <Container>
