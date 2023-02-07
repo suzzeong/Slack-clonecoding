89a04172eeab727da2581725ad95959c4edbda1f
suzzeong <su981006@naver.com>
2023-02-07 21:17:07 +0900

    feat: custom-scrollbars, dayjs 추가

--- front/components/Chat/index.tsx
+++ front/components/Chat/index.tsx
@@ -2,6 +2,7 @@ import React, { VFC } from 'react';
 import { ChatWrapper } from '@components/Chat/styles';
 import gravatar from 'gravatar';
 import { IDM } from '@typings/db';
+import dayjs from 'dayjs';
 
 interface Props {
   data: IDM;
@@ -17,8 +18,8 @@ const Chat: VFC<Props> = ({ data }) => {
       </div>
       <div className="chat-text">
         <div className="chat-user">
-          <b>{user.nickname}</b>
-          <span>{data.createdAt}</span>
+          <b>{user.nickname} </b>
+          <span>{dayjs(data.createdAt).format('h:mm A')}</span>
         </div>
         <p>{data.content}</p>
       </div>
--- front/components/ChatList/index.tsx
+++ front/components/ChatList/index.tsx
@@ -1,18 +1,24 @@
 import { ChatZone, Section } from './styles';
-import React, { VFC } from 'react';
+import React, { useCallback, useRef, VFC } from 'react';
 import { IDM } from '@typings/db';
 import Chat from '@components/Chat';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 interface Props {
   chatData?: IDM[];
 }
 
 const ChatList: VFC<Props> = ({ chatData }) => {
+  const scrollbarRef = useRef(null);
+  const onScroll = useCallback(() => {}, []);
+
   return (
     <ChatZone>
-      {chatData?.map((chat) => (
-        <Chat key={chat.id} data={chat} />
-      ))}
+      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+        {chatData?.map((chat) => (
+          <Chat key={chat.id} data={chat} />
+        ))}
+      </Scrollbars>
     </ChatZone>
   );
 };
