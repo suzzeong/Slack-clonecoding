a6a7081a39cba0bbf193f1502e51127e9054bc77
suzzeong <su981006@naver.com>
2023-02-07 20:13:29 +0900

    feat: DM Chat 내용 만들기

---
+++ front/components/Chat/index.tsx
@@ -0,0 +1,29 @@
+import React, { VFC } from 'react';
+import { ChatWrapper } from '@components/Chat/styles';
+import gravatar from 'gravatar';
+import { IDM } from '@typings/db';
+
+interface Props {
+  data: IDM;
+}
+
+const Chat: VFC<Props> = ({ data }) => {
+  const user = data.Sender;
+
+  return (
+    <ChatWrapper>
+      <div className="chat-img">
+        <img src={gravatar.url(user.email, { s: '36px', d: 'retro' })} alt={user.nickname} />
+      </div>
+      <div className="chat-text">
+        <div className="chat-user">
+          <b>{user.nickname}</b>
+          <span>{data.createdAt}</span>
+        </div>
+        <p>{data.content}</p>
+      </div>
+    </ChatWrapper>
+  );
+};
+
+export default Chat;
--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -51,7 +51,7 @@ const DirectMessage = () => {
         <img src={gravatar.url(userData.email, { s: '28px', d: 'retro' })} alt={userData.nickname} />
         <span>{userData.nickname}</span>
       </Header>
-      <ChatList />
+      <ChatList chatData={chatData} />
       <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
   );
