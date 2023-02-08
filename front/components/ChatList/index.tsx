27ba45fcf655324ab43ed9567277da2a052488f4
suzzeong <su981006@naver.com>
2023-02-08 22:50:11 +0900

    feat: 리버스 인피니트 스크롤링(useSWRInfinite)

--- front/components/ChatList/index.tsx
+++ front/components/ChatList/index.tsx
@@ -1,35 +1,45 @@
 import { ChatZone, Section, StickyHeader } from '@components/ChatList/styles';
-import React, { useCallback, useRef, VFC } from 'react';
+import React, { forwardRef, useCallback, useRef, VFC } from 'react';
 import { IDM } from '@typings/db';
 import Chat from '@components/Chat';
 import { Scrollbars } from 'react-custom-scrollbars';
 
 interface Props {
   chatSections: { [key: string]: IDM[] };
+  setSize: (f: (size: number) => number) => Promise<IDM[][] | undefined>;
+  isEmpty: boolean;
+  isReachingEnd: boolean;
 }
 
-const ChatList: VFC<Props> = ({ chatSections }) => {
-  const scrollbarRef = useRef(null);
-  const onScroll = useCallback(() => {}, []);
+const ChatList = forwardRef<Scrollbars, Props>(({ chatSections, setSize, isEmpty, isReachingEnd }, ref) => {
+  const onScroll = useCallback((values) => {
+    if (values.scrollTop === 0 && !isReachingEnd) {
+      console.log('가장 위');
+      setSize((preSize) => preSize + 1)
+      .then(()=>{
+        // 스크롤 위치 유지
+      })
+    }
+  }, []);
 
   return (
     <ChatZone>
-      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+      <Scrollbars autoHide ref={ref} onScrollFrame={onScroll}>
         {Object.entries(chatSections).map(([date, chat]) => {
           return (
             <Section className={`section-${date}`} key={date}>
               <StickyHeader>
                 <button>{date}</button>
               </StickyHeader>
-              {chat.map((chat)=>(
+              {chat.map((chat) => (
                 <Chat key={chat.id} data={chat} />
               ))}
             </Section>
-          )
-          })}
+          );
+        })}
       </Scrollbars>
     </ChatZone>
   );
-};
+});
 
 export default ChatList;
--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -1,5 +1,5 @@
 import { Container, Header } from '@pages/DirectMessage/styles';
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import gravatar from 'gravatar';
 import { IDM, IUser } from '@typings/db';
 import fetcher from '@utils/fetcher';
@@ -10,15 +10,25 @@ import ChatList from '@components/ChatList';
 import useInput from '@hooks/useInput';
 import axios from 'axios';
 import makeSection from '@utils/makeSection';
+import Scrollbars from 'react-custom-scrollbars';
+import useSWRInfinite from 'swr/infinite';
 
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace?: string; id: string }>();
   const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
   const { data: myData } = useSWR(`/api/users`, fetcher);
-  const { data: chatData, mutate: mutateChat } = useSWR<IDM[]>(
-    `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`,
+  const {
+    data: chatData,
+    mutate: mutateChat,
+    setSize,
+  } = useSWRInfinite<IDM[]>(
+    (index) => `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=${index + 1}`,
     fetcher,
   );
+
+  const isEmpty = chatData?.[0]?.length === 0;
+  const isReachingEnd = isEmpty || (chatData && chatData[chatData.length - 1]?.length < 20) || false;
+  const scrollbarRef = useRef<Scrollbars>(null);
 
   const [chat, onChangeChat, setChat] = useInput('');
 
@@ -46,7 +56,7 @@ const DirectMessage = () => {
     return null;
   }
 
-  const chatSections = makeSection(chatData ? [...chatData].reverse() : [])
+  const chatSections = makeSection(chatData ? chatData.flat().reverse() : []);
 
   return (
     <Container>
@@ -54,7 +64,13 @@ const DirectMessage = () => {
         <img src={gravatar.url(userData.email, { s: '28px', d: 'retro' })} alt={userData.nickname} />
         <span>{userData.nickname}</span>
       </Header>
-      <ChatList chatSections={chatSections} />
+      <ChatList
+        chatSections={chatSections}
+        ref={scrollbarRef}
+        setSize={setSize}
+        isEmpty={isEmpty}
+        isReachingEnd={isReachingEnd}
+      />
       <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
   );
