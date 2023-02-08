e044426d9ea58f8cdf161fc1c68475a54a61d651
suzzeong <su981006@naver.com>
2023-02-08 22:08:03 +0900

    feat: DM 날짜별로 묶어주기

--- front/components/ChatList/index.tsx
+++ front/components/ChatList/index.tsx
@@ -1,23 +1,32 @@
-import { ChatZone, Section } from './styles';
+import { ChatZone, Section, StickyHeader } from '@components/ChatList/styles';
 import React, { useCallback, useRef, VFC } from 'react';
 import { IDM } from '@typings/db';
 import Chat from '@components/Chat';
 import { Scrollbars } from 'react-custom-scrollbars';
 
 interface Props {
-  chatData?: IDM[];
+  chatSections: { [key: string]: IDM[] };
 }
 
-const ChatList: VFC<Props> = ({ chatData }) => {
+const ChatList: VFC<Props> = ({ chatSections }) => {
   const scrollbarRef = useRef(null);
   const onScroll = useCallback(() => {}, []);
 
   return (
     <ChatZone>
       <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
-        {chatData?.map((chat) => (
-          <Chat key={chat.id} data={chat} />
-        ))}
+        {Object.entries(chatSections).map(([date, chat]) => {
+          return (
+            <Section className={`section-${date}`} key={date}>
+              <StickyHeader>
+                <button>{date}</button>
+              </StickyHeader>
+              {chat.map((chat)=>(
+                <Chat key={chat.id} data={chat} />
+              ))}
+            </Section>
+          )
+          })}
       </Scrollbars>
     </ChatZone>
   );
--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -9,6 +9,7 @@ import ChatBox from '@components/ChatBox';
 import ChatList from '@components/ChatList';
 import useInput from '@hooks/useInput';
 import axios from 'axios';
+import makeSection from '@utils/makeSection';
 
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace?: string; id: string }>();
@@ -45,13 +46,15 @@ const DirectMessage = () => {
     return null;
   }
 
+  const chatSections = makeSection(chatData ? [...chatData].reverse() : [])
+
   return (
     <Container>
       <Header>
         <img src={gravatar.url(userData.email, { s: '28px', d: 'retro' })} alt={userData.nickname} />
         <span>{userData.nickname}</span>
       </Header>
-      <ChatList chatData={chatData} />
+      <ChatList chatSections={chatSections} />
       <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
   );
