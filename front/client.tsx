1d3f872c96aec8753143b62ae0b061e5c139b390
suzzeong <su981006@naver.com>
2023-02-11 21:03:57 +0900

    add: SWR Devtools 추가

--- front/client.tsx
+++ front/client.tsx
@@ -2,10 +2,22 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from '@layouts/App';
+import SWRDevtools from '@jjordy/swr-devtools';
+import axios from 'axios';
 
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL =
+  process.env.NODE_ENV === 'production' ? 'https://sleact.nodebird.com' : 'http://localhost:3090';
+console.log('env', process.env.NODE_ENV === 'production');
 render(
   <BrowserRouter>
-    <App />
+    {process.env.NODE_ENV === 'production' ? (
+      <App />
+    ) : (
+      <SWRDevtools>
+        <App />
+      </SWRDevtools>
+    )}
   </BrowserRouter>,
   document.querySelector('#app'),
 );
